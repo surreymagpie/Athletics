0e72549015e6570c07d4dc95ad8805f89ecf3443
Rob Curtis <surreymagpie@yahoo.co.uk>
2014-06-18 08:26:52 +0100

    Working datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery.ui.datepicker
 //= require turbolinks
 //= require_tree .
 
 $(function () {
-  $('.datepicker').datepicker()
+  $('.datepicker').datepicker({
+    dateFormat: 'dd/mm/yy'
+  })
 })
