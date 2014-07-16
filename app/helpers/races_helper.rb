57b320b85a9c66129d86e3e5a314c99ebf4d4259
Rob Curtis <surreymagpie@yahoo.co.uk>
2014-07-16 17:20:23 +0100

    Don't display score tables if no results yet

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,24 +1,22 @@
 module RacesHelper
   def table_race_scores(race)
+    return unless race.race_scores.any?
     @team_tables, table_titles = [], []
-
     if race.score_by_division?
       divs = Race.get_divisions(race)
       divs.each do |div|
         @team_tables << RaceScore.joins(:club).where('race_id = ? AND clubs.division = ?', race.id, div).order('total ASC').to_a
         table_titles << content_tag(:caption, "Division #{div}")
-      end      
+      end
     elsif race.score_by_category?
       cats = Race.get_categories(race)
       cats.each do |cat|
         @team_tables << RaceScore.where('race_id = ? AND team_name ILIKE ?', race.id, "%#{cat}%").order('total ASC').to_a
         table_titles << content_tag(:caption, "#{cat} Category")
       end
-      # render partial: 'team_table', collection: @team_tables, locals: { table_title: table_titles}
     else
       @team_tables << RaceScore.where(race_id: race.id).order('total ASC').to_a
       table_titles << content_tag(:caption, "All results")
-      # render partial: 'team_table', collection: @team_tables, locals: { team_table: @team_table, table_title: Array(content_tag(:caption, "All results"))}
     end
       render partial: 'team_table', collection: @team_tables, locals: { table_titles: table_titles}
   end
